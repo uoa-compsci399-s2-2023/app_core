adda9aeffeaa51aee65837ea9918e3ac8935c92f
allan <97867490+ccamel55@users.noreply.github.com>
2023-08-17 01:46:33 +1200

    ui: add gluestack ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as gs from "@gluestack-ui/react";
+import * as rn from "react-native";
 
 import React from "react";
 import registerRootComponent from "expo/build/launch/registerRootComponent";
 
-export default function App() {
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <gs.GluestackUIProvider config={gs.config.theme}>
+      <gs.Center bg='#101010' h='100%' w='100%'>
+        <rn.View flexDirection='column' gap={20} width='70%'>
+          <gs.Text textAlign='center' color='#909090'
+          >Hey there, Delilah What's it like in New York city? {"\n"}
+                        I'm a thousand miles away But, girl, tonight you look so pretty {"\n"}
+                        Yes, you do  {"\n"}
+                        Time square can't shine as bright as you I swear, it's true
+          </gs.Text>
+          <gs.Spinner size="large" />
+          <gs.Button
+            size="md"
+            variant="solid"
+            action="primary"
+          >
+            <gs.ButtonText>Press Me</gs.ButtonText>
+          </gs.Button>
+        </rn.View>
+      </gs.Center>
+    </gs.GluestackUIProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    backgroundColor: '#fff',
-    flex: 1,
-    justifyContent: 'center',
-  },
-});
 
 registerRootComponent(App);

15ec8f3d5e998afdb79cd5e46f42f000a02d6bfc
allan <97867490+ccamel55@users.noreply.github.com>
2023-08-17 01:45:29 +1200

    lint: change unwanted rules
    - removed rules which conflict with gluestack ui and rules that are not needed

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,10 +18,10 @@ module.exports = {
         "plugin:react/recommended"
     ],
     "rules": {
+        "react/prop-types": 0,
         "react-native/no-unused-styles": 2,
         "react-native/split-platform-components": 2,
         "react-native/no-inline-styles": 2,
-        "react-native/no-raw-text": 2,
         "react-native/sort-styles": [
             "error",
             "asc",

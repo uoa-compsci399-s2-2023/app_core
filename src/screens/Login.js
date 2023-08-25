6a9723b0c77c74b004ddf6118fbcb640c7b51ef5
Hashbrowns <aaron.tse5801@outlook.com>
2023-08-26 01:12:27 +1200

    sign-in-develop-level

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,19 +1,48 @@
-import React from "react";
-
-import {View, Image, StyleSheet} from "react-native";
+import * as React from "react";
+import * as SecureStore from 'expo-secure-store';
+import { View, Image, StyleSheet} from "react-native";
 import {LoginButton} from "../components/Buttons";
 import { Screen } from "../components/Layout";
 
-export default function Login({ navigation }) {
+import {exchangeCodeAsync, makeRedirectUri, useAuthRequest, useAutoDiscovery, } from "expo-auth-session";
 
-  const styles = StyleSheet.create({
-    view: {
-      alignItems: "center",
-      height: "100%",
-      justifyContent: "center",
-      width: "100%",
-    }
+export default function Login({ navigation}) {
+  
+  // Endpoint
+  const discovery = useAutoDiscovery(
+    "https://login.microsoftonline.com/common/v2.0",
+  );
+
+  const redirectUri = makeRedirectUri({
+    native: "exp://redirect",
+    preferLocalhost: true, //Temp
   });
+
+  const clientId = '7644da13-4be5-4c5c-860e-f068183aa631';
+
+  const [request,, promptAsync] = useAuthRequest(
+    {
+      clientId,
+      scopes: ['User.Read','openid', 'profile', 'email', 'offline_access'],
+      redirectUri,
+    },
+    discovery,
+  );
+
+  const [accessToken, setAccessToken] = React.useState(String|null);
+
+  React.useEffect(() => {
+    async function currentAccessToken() {
+      const accessToken  = await SecureStore.getItemAsync('accessToken');
+      setAccessToken(accessToken);
+    }
+    currentAccessToken()
+  }, [])
+
+  const saveAccessToken = async (accessToken) => {
+    setAccessToken(accessToken);
+    await SecureStore.setItemAsync('accessToken', accessToken);
+  }
 
   return (
     <Screen>
@@ -24,11 +53,41 @@ export default function Login({ navigation }) {
           text="Sign in with Microsoft" 
           icon={require("../../assets/icons/microsoft.png")}
           onPress={() =>
-            navigation.navigate('Scan')
+            promptAsync().then((codeResponse) => {
+              if (request && codeResponse?.type === 'success' && discovery) {
+                exchangeCodeAsync(
+                  {
+                    clientId,
+                    code: codeResponse.params.code,
+                    extraParams: request.codeVerifier
+                      ? { code_verifier: request.codeVerifier }
+                      : undefined,
+                    redirectUri,
+                  },
+                  discovery,
+                ).then((res) => {
+                  setAccessToken(res.accessToken); //has refresh token in res
+                  saveAccessToken(accessToken);
+                  navigation.navigate('Scan')
+                });
+              }
+            })
           }
         />
-        <LoginButton text="Sign in with Google" icon={require("../../assets/icons/google.png")}/>
+        <LoginButton text="Sign in with Google" icon={require("../../assets/icons/google.png")}
+          onPress={() =>
+            navigation.navigate('Scan')
+          }/>
       </View>
     </Screen>
   );
 }
+
+const styles = StyleSheet.create({
+  view: {
+    alignItems: "center",
+    height: "100%",
+    justifyContent: "center",
+    width: "100%",
+  }
+});

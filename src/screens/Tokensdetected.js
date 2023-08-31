8ae715ccdf3be90598c70296c63380a1e257e159
JT <jt.apriori@gmail.com>
2023-09-01 11:24:18 +1200

    handled scrollable behaviour and text wrap

--- src/screens/Tokensdetected.js
+++ src/screens/Tokensdetected.js
@@ -1,17 +1,17 @@
 import React, {useRef, useState} from 'react';
-import {View, Text, StyleSheet, TextInput,TouchableOpacity} from 'react-native';
+import {View, Text, StyleSheet, TextInput,TouchableOpacity, ScrollView} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { Screen } from "../components/Layout";
 
-
-
-
 export default function TokensDetected({ route}) {
+  // NOTE: Used placeholderNotes for testing
+  // const placeholderNotes = "Title:     (Oh, What A Night) @December1963 Oh, what a night/nLate December, back in '63/n/nWhat a very special time for me/nAs I remember, what a night/nOh, what a night/nYou know, I didn't even/n know her name/nBut I was never gonna be the same/nWhat a lady, what a night/nOh, I, I got a funny feeling/nWhen she walked in the room/nAnd my, as I recall/nIt ended much too soon/n/n(Oh, what a night)/nHypnotizing, mesmerizin' me/nShe was everything I dreamed she'd be/nSweet surrender, what a night/n/nAnd I felt a rush/nLike a rolling bolt of thunder/nSpinning my head around/nAnd takin' my body under/n(Oh, what a night)/nOh, I got a funny feeling/nWhen she walked in the room/nAnd my, as I recall/nIt ended much too soon/n/n(Oh, what a night)/nWhy'd it take so long to see the light?/nSeemed so wrong, but now it seems so right/nWhat a lady, what a night/n/nOh, I felt a rush/nLike a rolling bolt of thunder/nSpinning my head around/nAnd takin' my body under/n/n(Oh, what a night)/nDo do do, do do, do do do do/n(Oh, what a night)/nDo do do, do do, do do do do/n(Oh, what a night)/nDo do do, do do, do do do do/n(Oh, what a night)/nDo do do, do do, do do do do.../n"
   const { scannedText } = route.params;
-  const inputRef = useRef(null);
-
-
   
+  const inputRef = useRef(null);
+  
+  // NOTE: Used placeholderNotes for testing
+  // const fileName = placeholderNotes.split(/\r?\n/);
   const fileName = scannedText.split(/\r?\n/);
   let cleanedTitle = "";
   let cleanedAgenda = "";
@@ -19,14 +19,6 @@ export default function TokensDetected({ route}) {
   const title = fileName.find(element => element.includes("Title"));
   const folder = fileName.findIndex(element => element.includes("Folder"));
   const date = fileName.findIndex(element => element.includes("@"));
-
-
-
-
-
-
-  
-  
   
   if (title) {
     const colonIndex = title.indexOf(":");
@@ -45,7 +37,6 @@ export default function TokensDetected({ route}) {
     cleanedDate = dateElement.substr(1).trim(); 
   }
 
-
   const [isEditingFile, setIsEditingFIle] = useState(false);
   const [isEditingAgen, setIsEditingAgen] = useState(false);
   const [fileNameText, setText] = useState(cleanedTitle + "-" + cleanedDate ); 
@@ -57,25 +48,18 @@ export default function TokensDetected({ route}) {
     }
   };
 
-
   const editFileName = () => {
     setIsEditingFIle(!isEditingFile);
   };
-
   
   const editAgenda = () => {
     setIsEditingAgen(!isEditingAgen);
   };
-
-
-  
-
-
-
-  
+ 
   return (
     <Screen>
-      <View>
+      {/* Changed to ScrollView -- ScrollView handles overflows for anything enclosed in the component*/}
+      <ScrollView>
         <View style={styles.titleContainer}>
           <Text style={styles.titleText}>FileName:</Text>
           <View style={styles.buttonContainer}>
@@ -84,15 +68,23 @@ export default function TokensDetected({ route}) {
         </View>
         <View style={styles.dataContainer}>
           {isEditingFile ? (
-            <TextInput 
-              ref={inputRef}
-              style={styles.input}
-              value={fileNameText}
-              onChangeText={(newText) => setText(newText)}
-              autoFocus
-            />
+            // Added ScrollView
+            <ScrollView style={styles.textInputFormat}>
+              <TextInput 
+                ref={inputRef}
+                style={styles.input}
+                value={fileNameText}
+                onChangeText={(newText) => setText(newText)}
+                // Multiline allows entire fileNameText to fill in textBox
+                multiline
+                autoFocus
+              />
+            </ScrollView>
           ) : (
-            <Text>{fileNameText}</Text>
+            // Added ScrollView
+            <ScrollView>
+              <Text>{fileNameText}</Text>
+            </ScrollView>
           )}
           {isEditingFile ? (
             <TouchableOpacity onPress={editFileName}>
@@ -100,43 +92,45 @@ export default function TokensDetected({ route}) {
             </TouchableOpacity>
           ) : null}
         </View>
-      </View>
 
-
-
-
-      <View>
-        <View style={styles.titleContainer}>
-          <Text style={styles.titleText}>Agenda:</Text>
-          <View style={styles.buttonContainer}>
-            <Icon name="edit" size={25} color="black" style={styles.icon} onPress={editAgenda} />
+        <View>
+          <View style={styles.titleContainer}>
+            <Text style={styles.titleText}>Agenda:</Text>
+            <View style={styles.buttonContainer}>
+              <Icon name="edit" size={25} color="black" style={styles.icon} onPress={editAgenda} />
+            </View>
+          </View>
+          <View style={styles.dataContainer}>
+            {isEditingAgen ? (
+              // Added ScrollView 
+              <ScrollView>
+                <TextInput
+                  ref={inputRef}
+                  style={styles.dataContainer}
+                  value={agendaText}
+                  onChangeText={(newText) => setAgendaText(newText)}
+                  multiline={true}
+                  autoFocus
+                />
+              </ScrollView>
+            ) : (
+              // Added ScrollView
+              <ScrollView>
+                <Text>{agendaText}</Text>
+              </ScrollView>
+            )}
+            {isEditingAgen ? (
+              <TouchableOpacity onPress={editAgenda}>
+                <Text>Edit</Text>
+              </TouchableOpacity>
+            ) : null}
           </View>
         </View>
-        <View style={styles.dataContainer}>
-          {isEditingAgen ? (
-            <TextInput
-              ref={inputRef}
-              style={styles.dataContainer}
-              value={agendaText}
-              onChangeText={(newText) => setAgendaText(newText)}
-              autoFocus
-            />
-          ) : (
-            <Text>{agendaText}</Text>
-          )}
-          {isEditingAgen ? (
-            <TouchableOpacity onPress={editAgenda}>
-              <Text>Edit</Text>
-            </TouchableOpacity>
-          ) : null}
-        </View>
-      </View>
-    
 
-      <View style={styles.nextIconContainer}>
-        <Icon name="arrow-circle-o-right" size={100} color="black" />
-      </View>
-    
+        <View style={styles.nextIconContainer}>
+          <Icon name="arrow-circle-o-right" size={100} color="black" />
+        </View>
+      </ScrollView>
     </Screen>
   );
 }
@@ -149,7 +143,6 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     paddingHorizontal: 10,
     paddingVertical: 15,
-    
   },
 
   icon: {
@@ -167,6 +160,7 @@ const styles = StyleSheet.create({
     marginTop: 10,
     paddingLeft: 25,
   },
+
   titleText: {
     borderRadius: 10,
     fontSize: 16,

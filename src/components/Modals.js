1b002666f7d8e5229eed7ec62dc4babef7ab9c60
allan <97867490+ccamel55@users.noreply.github.com>
2023-09-01 00:23:49 +1200

    modal: add text input modal

--- src/components/Modals.js
+++ src/components/Modals.js
@@ -1,46 +1,52 @@
-import React from "react";
-import {Modal, StyleSheet, Text, View} from "react-native";
+import React, {useState} from "react";
+import {Modal, StyleSheet, Text, TextInput, View} from "react-native";
 import {lightTheme} from "../Theme.js";
 
-export function Alert(props) {
+const styles= StyleSheet.create({
+  actionText: {
+    color: lightTheme.accentColor,
+  },
+  actionView: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "80%"
+  },
+  modal: {
+    alignItems: "center",
+    backgroundColor: lightTheme.background,
+    borderRadius: 14,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    padding: 20,
+    rowGap: 20,
+    width: 320
+  },
+  modalText: {
+    textAlign: "center",
+  },
+  textInput: {
+    borderBottomWidth: 1,
+    height: 30,
+    padding: 5,
+    width: 250,
+  },
+  titleText: {
+    fontWeight: "bold"
+  },
+  view: {
+    alignItems: "center",
+    backgroundColor: lightTheme.backgroundFocused,
+    display: "flex",
+    height: "100%",
+    justifyContent: "center",
+    position: "absolute",
+    width: "100%"
+  }
+});
 
-  const styles= StyleSheet.create({
-    actionText: {
-      color: lightTheme.accentColor,
-    },
-    actionView: {
-      display: "flex",
-      flexDirection: "row",
-      justifyContent: "space-between",
-      width: "80%"
-    },
-    modal: {
-      alignItems: "center",
-      backgroundColor: lightTheme.background,
-      borderRadius: 14,
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "space-between",
-      padding: 20,
-      rowGap: 20,
-      width: 320
-    },
-    modalText: {
-      textAlign: "center",
-    },
-    titleText: {
-      fontWeight: "bold"
-    },
-    view: {
-      alignItems: "center",
-      backgroundColor: lightTheme.backgroundFocused,
-      display: "flex",
-      height: "100%",
-      justifyContent: "center",
-      position: "absolute",
-      width: "100%"
-    }
-  });
+export function Alert(props) {
 
   if (!props.visible) {
     return <View></View>;
@@ -60,4 +66,35 @@ export function Alert(props) {
       </View>
     </Modal>
   );
+}
+
+export function Textbox(props) {
+
+  /* Do we allow the programmer to register a onChangeText callback or should we pass the
+  * string back on 'ok'?  */
+  const [text, setText] = useState("");
+
+  if (!props.visible) {
+    return <View></View>;
+  }
+
+  const onConfirm = (string) => {
+    props.onConfirm(string);
+    setText("");
+  }
+
+  return (
+    <Modal animationType="fade" hardwareAccelerated={true} transparent={true}>
+      <View style={styles.view}>
+        <View style={styles.modal}>
+          <Text style={styles.titleText}>{props.modalTitle}</Text>
+          <TextInput style={styles.textInput} onChangeText={(currentText) => setText(currentText)} />
+          <View style={styles.actionView}>
+            <Text style={styles.actionText} onPress={() => onConfirm(null)}>Cancel</Text>
+            <Text style={styles.actionText} onPress={() => onConfirm(text)}>OK</Text>
+          </View>
+        </View>
+      </View>
+    </Modal>
+  )
 }

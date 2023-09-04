e418abd3493ed377027aca19dd88d2ed3f1ad14a
EternalError <longbuinz2001@gmail.com>
2023-09-04 20:53:24 +1200

    Add task with task promp and due date, fixing extracting due date and calculate date different

--- src/screens/Tokensdetected.js
+++ src/screens/Tokensdetected.js
@@ -16,6 +16,24 @@ export default function TokensDetected({ route}) {
   let cleanedTitle = "";
   let cleanedAgenda = "";
   let cleanedDate = "";
+ 
+  //have a list of task that extracted from the data where task will have # at the beginning and add the due datew to the task list start with @ 
+  let task = [];
+  const taskIndex = fileName.findIndex(element => element.includes("#"));
+  if (taskIndex !== -1) {
+    task = fileName.slice(taskIndex);
+  }
+
+  const taskList = task.map((item) => {
+    // Replace "@" with "Due at:"
+    const formattedItem = item.replace('@', 'Due at:');
+    return formattedItem.substr(1).trim();
+  });
+
+
+
+  
+
   const title = fileName.find(element => element.includes("Title"));
   const folder = fileName.findIndex(element => element.includes("Folder"));
   const date = fileName.findIndex(element => element.includes("@"));
@@ -55,6 +73,11 @@ export default function TokensDetected({ route}) {
   const editAgenda = () => {
     setIsEditingAgen(!isEditingAgen);
   };
+
+  const editTask = () => {
+    setIsEditingAgen(!isEditingAgen);
+  }
+
  
   return (
     <Screen>
@@ -93,6 +116,7 @@ export default function TokensDetected({ route}) {
           ) : null}
         </View>
 
+         
         <View>
           <View style={styles.titleContainer}>
             <Text style={styles.titleText}>Agenda:</Text>
@@ -127,13 +151,72 @@ export default function TokensDetected({ route}) {
           </View>
         </View>
 
+
+        <View>
+          <View style={styles.titleContainer}>
+            <Text style={styles.titleText}>Task:</Text>
+            <View style={styles.buttonContainer}>
+              <Icon name="edit" size={25} color="black" style={styles.icon} onPress={editTask} />
+            </View>
+          </View>
+          <View style={styles.dataContainer}>
+            {/* Check if tasks exist */}
+            {taskList.length > 0 ? (
+            // Display the task list
+              taskList.map((item, index) => {
+
+                //  format  "DD-MM-YYYY":
+                const dueDate = item.match(/\d{2}-\d{2}-\d{4}/);
+                const formattedDueDate = dueDate;
+
+                
+                // Calculate the time difference between now and the due date
+                const currentDate = Date.now();
+                const taskDueDate = new Date(currentDate);
+                const dateOk = dueDate;
+
+                const timeDifferenceInDays = Math.floor((formattedDueDate + 1));// / (1000 * 60 * 60 * 24));
+                let indicatorColor = 'green';
+                if (timeDifferenceInDays >= 1 && timeDifferenceInDays <= 2) {
+                  indicatorColor = 'red';
+                }
+                else if (timeDifferenceInDays >= 3 && timeDifferenceInDays <= 5) {
+                  indicatorColor = 'orange';
+                ///else if there no due date or cant find due date auto set due date to 7 days 
+                } else if (timeDifferenceInDays >= 6 && timeDifferenceInDays <= 7 || timeDifferenceInDays === undefined || timeDifferenceInDays === null || timeDifferenceInDays < 0) {
+                  indicatorColor = 'green';
+
+                }
+                return (
+                  <View key={index} style={styles.taskContainer}>
+                    <View style={styles.taskTitleContainer}>
+                      <Text style={styles.taskTitleText}>{item}</Text>
+                      <View style={styles.taskIndicatorContainer}>
+                        <View style={[styles.taskIndicator, {backgroundColor: indicatorColor}]}></View>
+
+                        <Text style={styles.taskIndicatorText}>{timeDifferenceInDays} days</Text>
+                      </View>
+                    </View>
+                  </View>
+                );
+              })
+            ) : (
+            // Display a message if there are no tasks
+              <Text>No tasks found</Text>
+            )}
+          </View>
+        </View>
+
         <View style={styles.nextIconContainer}>
           <Icon name="arrow-circle-o-right" size={100} color="black" />
         </View>
+
+        
       </ScrollView>
     </Screen>
   );
 }
+
 
 
 const styles = StyleSheet.create({
@@ -150,6 +233,39 @@ const styles = StyleSheet.create({
     padding: 10,
   },
 
+  
+  taskContainer: {
+    backgroundColor: '#ffffff',
+    borderRadius: 10,
+    marginBottom: 20,
+    padding: 10,
+  },
+
+  taskIndicator: {
+    borderRadius: 5,
+    height: 10,
+    marginRight: 5,
+    width: 10,
+  },
+
+  taskIndicatorContainer: {
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  taskIndicatorText: {
+    color: 'gray',
+    fontSize: 12,
+  },
+  taskTitleContainer: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 10,
+  },
+  taskTitleText: {
+    flex: 1,
+    fontWeight: 'bold',
+  },
   titleContainer: {
     alignItems: 'center', 
     backgroundColor: '#e0e0e0', 
@@ -160,7 +276,6 @@ const styles = StyleSheet.create({
     marginTop: 10,
     paddingLeft: 25,
   },
-
   titleText: {
     borderRadius: 10,
     fontSize: 16,
@@ -168,8 +283,9 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     marginTop: 10,
   },
-  
 });
+  
+
 
 
 

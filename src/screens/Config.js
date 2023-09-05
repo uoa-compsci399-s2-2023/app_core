975e5043ad4da5487c88b4df03a75ae93215b5e8
JT <jt.apriori@gmail.com>
2023-09-06 00:46:00 +1200

    toggle credential key visibility. Styling changes

--- src/screens/Config.js
+++ src/screens/Config.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, TextInput, Text, Button } from 'react-native';
+import { StyleSheet, TextInput, Text, Button, View, TouchableOpacity } from 'react-native';
 import * as SecureStore from 'expo-secure-store';
 
 import { Screen } from "../components/Layout";
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function Config({ navigation }) {
 
   const [awsAccessKeyId, setAwsAccessKeyId] = useState('');
   const [awsSecretAccessKey, setAwsSecretAccessKey] = useState('');
+  const [showAccessKey, setShowAccessKey] = useState(false);
   const placeholderText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
   useEffect(() => {
@@ -26,6 +28,10 @@ export default function Config({ navigation }) {
     navigation.goBack();
   }
 
+  const toggleKeyVisibility = () => {
+    setShowAccessKey(!showAccessKey);
+  }
+
   return (
     <Screen>
       <Button
@@ -35,31 +41,83 @@ export default function Config({ navigation }) {
           })}
         title="Results Screen Bypass"
       />
-      <Text>AWS Access Key ID: </Text>
-      <TextInput
-        style={styles.input}
-        onChangeText={setAwsAccessKeyId}
-        value={awsAccessKeyId}
-      />
-      <Text>AWS Secret Access Key: </Text>
-      <TextInput
-        style={styles.input}
-        onChangeText={setAwsSecretAccessKey}
-        value={awsSecretAccessKey}
-      />
-      <Button
+      <View>
+        <Text style={styles.inputField}>AWS Access Key ID</Text>
+        <TextInput
+          style={styles.inputContainer}
+          onChangeText={setAwsAccessKeyId}
+          value={awsAccessKeyId}
+        />
+        <Text style={styles.inputField}>AWS Secret Access Key</Text>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.inputKey}
+            placeholder="Enter your Access Key"
+            secureTextEntry={!showAccessKey}
+            onChangeText={setAwsSecretAccessKey}
+            value={awsSecretAccessKey}
+          />
+          <TouchableOpacity onPress={toggleKeyVisibility}>
+            <Icon
+              name={showAccessKey ? 'eye-slash' : 'eye'}
+              type="font-awesome"
+              size={24}
+              style={styles.icon}
+              color={'grey'}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      <TouchableOpacity
         onPress={persistConfig}
-        title="Submit"
-      />
+        style={styles.button}
+      >
+        <Text style={styles.buttonText}>Submit</Text>
+      </TouchableOpacity>
     </Screen>
   );
 }
 
 const styles = StyleSheet.create({
-  input: {
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#3E989B',
+    marginTop: 40,
+    padding: 16,
+  },
+
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold'
+  },
+
+  icon: {
+    marginLeft: 20,
+    marginRight: 5
+  },
+
+  inputContainer: {
+    alignItems: 'center', 
+    borderColor: 'grey',
+    borderRadius: 5,
     borderWidth: 1,
+    flexDirection: 'row', 
     height: 40,
-    margin: 12,
+    paddingHorizontal: 10,
+  },
+
+  inputField: {
+    color: 'grey',
+    fontSize: 18,
+    marginTop: 14,
     padding: 10,
   },
+
+  inputKey: {
+    flex: 1,
+    height: 40,
+  },
+
 });

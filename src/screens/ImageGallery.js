fb67d530f4bd08ddfd94663d5124023ea5584a69
allan <97867490+ccamel55@users.noreply.github.com>
2023-09-07 19:19:45 +1200

    image gallery: impl textract

--- src/screens/ImageGallery.js
+++ src/screens/ImageGallery.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Spinner from 'react-native-loading-spinner-overlay';
 
 import {Image, View, StyleSheet, ScrollView, TouchableOpacity} from 'react-native'
+import {Buffer} from 'buffer';
+
+import textract from "../textract.js";
+import ScannedNote from "../models/ScannedNote.js";
 
 import {lightTheme, scaledSize, SCREEN_HEIGHT} from "../Theme.js";
+import {Screen} from "../components/Layout";
 import {TabsButton} from "../components/Buttons.js";
 
 export default function ImageGallery({ route, navigation }) {
 
   const {photos} = route.params;
+
+  const [showSpinner, setShowSpinner] = useState(false);
+  const [spinnerString, setSpinnerString] = useState("");
 
   const imagePreview = (i) => {
 
@@ -35,21 +44,58 @@ export default function ImageGallery({ route, navigation }) {
         </View>
       );
     }
+
     return thumbnails;
   };
 
+  const processTextract = async () => {
+
+    setSpinnerString(`Processing...`);
+    setShowSpinner(true);
+
+    let textractResponseString = "";
+
+    for (let i = 0; i < photos.length; i++) {
+
+      const photo = photos.at(i);
+      setSpinnerString(`Processing: ${i + 1}/${photos.length}...`);
+
+      const response = await textract.detectDocumentText({
+        data: Buffer.from(photo.base64, 'base64'),
+        credentials: { accessKeyId: process.env.AWS_ACCESS_KEY_ID, secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY}
+      });
+
+      textractResponseString += ScannedNote.fromTextractResponse(response).text + "\n";
+    }
+
+    // return states back to normal
+    setShowSpinner(false);
+
+    navigation.navigate('Scan Result', {
+      scannedText:  textractResponseString,
+    })
+  }
+
   return (
-    <View style={styles.view}>
-      <ScrollView contentContainerStyle={styles.scrollView}>
-        {renderThumbnails()}
-      </ScrollView>
-      <View style={styles.footer}>
-        <View style={styles.row}>
-          <TabsButton text={"Cancel"} onPress={() => navigation.navigate("Login")}/>
-          <TabsButton text={"Confirm"}/>
+    <Screen>
+      <Spinner
+        visible={showSpinner}
+        textContent={spinnerString}
+        textStyle={styles.spinnerText}
+      />
+
+      <View style={styles.view}>
+        <ScrollView contentContainerStyle={styles.scrollView}>
+          {renderThumbnails()}
+        </ScrollView>
+        <View style={styles.footer}>
+          <View style={styles.row}>
+            <TabsButton text={"Cancel"} onPress={() => navigation.navigate("Login")}/>
+            <TabsButton text={"Confirm"} onPress={() => processTextract()}/>
+          </View>
         </View>
       </View>
-    </View>
+    </Screen>
   )
 }
 
@@ -88,6 +134,12 @@ const styles = StyleSheet.create({
     paddingBottom: "5%",
     paddingTop: "5%",
     width: "100%"
+  },
+  spinnerText: {
+    color: lightTheme.text,
+    fontSize: scaledSize(24),
+    fontWeight: "normal",
+    textAlign: "center"
   },
   view: {
     backgroundColor: lightTheme.backgroundDark,

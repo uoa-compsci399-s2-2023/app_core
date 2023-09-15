000540fa027319f7bf17da3aa24961329fe5a236
allan <97867490+ccamel55@users.noreply.github.com>
2023-09-15 22:12:53 +1200

    theme: fix scale size, move into function

--- src/Theme.js
+++ src/Theme.js
@@ -1,3 +1,5 @@
+import {Dimensions, Platform, PixelRatio} from "react-native";
+
 export const lightTheme = {
   background: "#FFFFFF",
   backgroundDark: "#2F2F2F",
@@ -11,3 +13,23 @@ export const lightTheme = {
   errorColor: "#fa3a40",
   importantColor: "#ff9900"
 };
+
+export const {
+  width: SCREEN_WIDTH,
+  height: SCREEN_HEIGHT
+} = Dimensions.get('window');
+
+// note: we can change the '400' to any scale factor we want for real time scaling.
+const scale = SCREEN_WIDTH / 400;
+
+export function scaledSize(size) {
+
+  const newSize = size * scale
+
+  if (Platform.OS === 'ios') {
+    return Math.round(PixelRatio.roundToNearestPixel(newSize))
+  }
+  else {
+    return Math.round(PixelRatio.roundToNearestPixel(newSize)) - 2
+  }
+}

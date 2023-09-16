ce3dbe87720bbea918788f9f7900bc42c848b1bf
EternalError <longbuinz2001@gmail.com>
2023-09-16 22:44:16 +1200

    Finish changing duedate, @next ....

--- src/models/Task.js
+++ src/models/Task.js
@@ -25,13 +25,52 @@ class Task {
 
   static fromScannedText(text) {
     const dueDate = text.split('@')[1]?.trim();
-    let name;
-    if(dueDate) {
-      name = text.match(/#(.*?)@/)[1].trim()
+  
+    if (dueDate && dueDate.includes('next')) {
+      let todayDate = new Date();
+      let nextDay = new Date(todayDate);
+  
+      if (dueDate.toLowerCase().includes('monday')) {
+        nextDay.setDate(todayDate.getDate() + ((7 - todayDate.getDay()) + 1) % 7);
+      } else if (dueDate.toLowerCase().includes('tuesday')) {
+        nextDay.setDate(todayDate.getDate() + ((7 - todayDate.getDay()) + 2) % 7);
+      } else if (dueDate.toLowerCase().includes('wednesday')) {
+        nextDay.setDate(todayDate.getDate() + ((7 - todayDate.getDay()) + 3) % 7);
+      } else if (dueDate.toLowerCase().includes('thursday')) {
+        nextDay.setDate(todayDate.getDate() + ((7 - todayDate.getDay()) + 4) % 7);
+      } else if (dueDate.toLowerCase().includes('friday')) {
+        nextDay.setDate(todayDate.getDate() + ((7 - todayDate.getDay()) + 5) % 7);
+      } else if (dueDate.toLowerCase().includes('saturday')) {
+        nextDay.setDate(todayDate.getDate() + ((7 - todayDate.getDay()) + 6) % 7);
+      } else if (dueDate.toLowerCase().includes('sunday')) {
+        nextDay.setDate(todayDate.getDate() + ((7 - todayDate.getDay()) + 7) % 7);
+      } else if (dueDate.includes('week')) {
+        nextDay.setDate(todayDate.getDate() + 7);
+      }
+  
+      const day = nextDay.getDate();
+      const month = nextDay.getMonth() + 1;
+      const year = nextDay.getFullYear();
+      const modifiedDueDate = `${day}/${month}/${year}`;
+  
+      let name;
+      if (dueDate) {
+        name = text.match(/#(.*?)@/)[1].trim();
+      } else {
+        name = text.split('#')[1].trim();
+      }
+  
+      return new Task({ name, dueDate: modifiedDueDate });
     } else {
-      name = text.split('#')[1].trim()
+      let name;
+      if (dueDate) {
+        name = text.match(/#(.*?)@/)[1].trim();
+      } else {
+        name = text.split('#')[1].trim();
+      }
+  
+      return new Task({ name, dueDate});
     }
-    return new Task({ name, dueDate })
   }
 
   static fromDisplayableText(text) {
@@ -53,10 +92,14 @@ class Task {
   get displayableDueDateText() {
     if(this.dueDate) {
       const difference = getTimeDifferenceInDays(this.dueDate);
-      return difference <= 0  ? 'Done' : `Due in ${difference} day${difference !== 1 ? 's' : ''}`;
+      if (difference === 0) {
+      return 'Due Today';
+      }
+    return difference < 0  ? 'Done' : `Due in ${difference} day${difference !== 1 ? 's' : ''}`;   
     }
     return undefined;
   }
+
 
   get displayableText() {
     if(this.dueDate) {
@@ -69,7 +112,7 @@ class Task {
     let indicatorColor = 'grey';
     if(this.dueDate) {
       const timeDifferenceInDays = getTimeDifferenceInDays(this.dueDate);
-      if (timeDifferenceInDays >= 1 && timeDifferenceInDays <= 2) {
+      if (timeDifferenceInDays >= 0 && timeDifferenceInDays <= 2) {
         indicatorColor = 'red';
       } else if (timeDifferenceInDays >= 3 && timeDifferenceInDays <= 5) {
         indicatorColor = 'orange';

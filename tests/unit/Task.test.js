5a7d4305eff005952fe5a98e29d1082e73eb2372
EternalError <longbuinz2001@gmail.com>
2023-09-23 21:23:59 +1200

    fixing error

--- tests/unit/Task.test.js
+++ tests/unit/Task.test.js
@@ -86,4 +86,32 @@ describe('Task', () => {
     });
   });
 
+  describe('calculateNextDueDate', () => {
+    it('should calculate the next due date for "next week"', () => {
+      // Mock today's date (Saturday)
+      const todayDate = new Date('2023-09-09T00:00:00Z'); // A Saturday
+      const dueDate = 'next week';
+      const nextDueDate = Task.calculateNextDueDate(todayDate, dueDate);
+      const expectedNextDueDate = new Date('2023-09-16T00:00:00Z'); // Saturday of the following week
+      expect(nextDueDate).toEqual(expectedNextDueDate);
+    });
+
+    it('should calculate the next due date for "next sunday"', () => {
+      const todayDate = new Date('2023-09-09T00:00:00Z'); // A Saturday
+      const dueDate = 'next sunday';
+      const nextDueDate = Task.calculateNextDueDate(todayDate, dueDate);
+      const expectedNextDueDate = new Date('2023-09-10T00:00:00Z'); // Sunday of this week
+      expect(nextDueDate).toEqual(expectedNextDueDate);
+    });
+
+    it('should calculate the next due date for "next monday"', () => {
+      const todayDate = new Date('2023-09-09T00:00:00Z'); // A Saturday
+      const dueDate = 'next monday';
+      const nextDueDate = Task.calculateNextDueDate(todayDate, dueDate);
+      const expectedNextDueDate = new Date('2023-09-11T00:00:00Z'); // Monday of this week
+      expect(nextDueDate).toEqual(expectedNextDueDate);
+    });
+  });
+
+
 });

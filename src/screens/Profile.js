c887412d55b57cdad080d97ea1af705fa0f5e89d
Raymond Yang <raymondyang@outlook.co.nz>
2023-10-05 22:49:22 +1300

    Add settings options to the Profile screen

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as SecureStore from 'expo-secure-store';
-import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
+import { View, StyleSheet, Text, TouchableOpacity, Switch, TextInput } from "react-native";
 import {Screen} from "../components/Layout";
 import Icon from 'react-native-vector-icons/FontAwesome';
 
@@ -41,6 +41,36 @@ export default function Login({ navigation}) {
             <Text style={styles.userNameText}>{ preferredUserName }</Text>
           </View>
         </View>
+        <View>
+          <Text style={styles.settingsHeader}>Settings</Text>
+          <View style={[styles.settingsItemContainer, styles.spaceBottom]}>
+            <Text style={styles.settingsItemHeader}>Dark Mode: </Text>
+            <Switch
+              value={false}
+              disabled={true}
+              trackColor={{false: '#767577', true: '#81b0ff'}}
+              thumbColor={'#f4f3f4'}
+              ios_backgroundColor="#3e3e3e"
+            />
+          </View>
+          <Text style={styles.settingsItemHeader}>Tokens: </Text>
+          <View style={styles.settingsItemContainer}>
+            <Text >File Name: </Text>
+            <TextInput style={styles.input} value="Title:" editable={false} />
+          </View>
+          <View style={styles.settingsItemContainer}>
+            <Text>Folder: </Text>
+            <TextInput style={styles.input} value="Folder:" editable={false} />
+          </View>
+          <View style={styles.settingsItemContainer}>
+            <Text>Task: </Text>
+            <TextInput style={styles.input} value="#" editable={false} />
+          </View>
+          <View style={styles.settingsItemContainer}>
+            <Text>Task Due Date: </Text>
+            <TextInput style={styles.input} value="@" editable={false} />
+          </View>
+        </View>
         <TouchableOpacity onPress={handleLogout}>
           <Text style={styles.logoutButton}>Logout</Text>
         </TouchableOpacity>
@@ -50,6 +80,14 @@ export default function Login({ navigation}) {
 }
 
 const styles = StyleSheet.create({
+  input: {
+    backgroundColor: '#EBEBE4',
+    borderWidth: 1,
+    height: 40,
+    margin: 12,
+    padding: 10,
+    width: '50%',
+  },
   logoutButton: {
     color: 'red',
     fontSize: 28,
@@ -61,6 +99,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     marginTop: 30,
   },
+  settingsHeader: {
+    fontSize: 28,
+    marginBottom: 20,
+  },
+  settingsItemContainer: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+  },
+  settingsItemHeader: {
+    fontSize: 20,
+  },
+  spaceBottom: {
+    marginBottom: 20,
+  },
   userNameText: {
     fontSize: 20,
     marginTop: 10,
@@ -68,6 +122,8 @@ const styles = StyleSheet.create({
   view: {
     height: "100%",
     justifyContent: "space-between",
+    paddingLeft: 20,
+    paddingRight: 20,
     width: "100%",
-  },
+  }
 });
